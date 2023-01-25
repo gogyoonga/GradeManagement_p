a1baddcf0c50fe747799d2144a5f74c7131cd48e
gogyoonga <ouna2@naver.com>
2023-01-25 12:00:11 +0900

    fist commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            System.out.println("반갑습니다. 권한을 선택해주세요.");
+            System.out.println("1.학생 2. 교수 / 그 외. 종료 \n >> ");
+            int select = sc.nextInt();
+
+            switch (select){
+                case 1:
+
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;
+
+            }
+
+        }
+
+        System.out.println("exe");
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Student {
+    String stuNo; //학번
+    String name; //이름
+    int year; //학년
+    HashMap<String, Integer> score = new HashMap<String, Integer>(); //과목 점수
+
+
+    public String getStuNo(){return stuNo;}
+    public void setStuNo(String stuNo){this.stuNo = stuNo;}
+    public String getName(){return name;}
+    public void setName(String name){ this.name = name;}
+    public int getYear() {return year;}
+    public void setYear(int year){this.year = year;}
+    public void getScore(){
+        score.forEach((sub, sco)->{
+            System.out.println(sub+" : "+sco);
+                }
+        );}
+
+    public void putScore(String sub) {
+        score.put(sub, 0);
+    }
+    public void removeScore(String sub){
+        score.remove(sub);
+    }
+    public void clearScore(){ score.clear();}
+
+
+}
